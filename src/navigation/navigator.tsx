035488ee702415e619882dcffdee05062b9ed62d
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-06-19 21:32:08 +0530

    configure vector icons

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -1,6 +1,7 @@
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {HomeStack, SearchStack} from './stacks';
 
 type RootTabParamList = {
@@ -13,17 +14,33 @@ const Tab = createMaterialBottomTabNavigator<RootTabParamList>();
 const AppNavigator = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator labeled={false}>
         <Tab.Screen
           name="homeTab"
           component={HomeStack}
-          // options={{
-          //   tabBarIcon: ({focused, color}) => (
-          //     <Icon name="bell" color={color} size={25} />
-          //   ),
-          // }}
+          options={{
+            tabBarIcon: ({focused, color}) => (
+              <Icon
+                name={focused ? 'home-circle' : 'home-circle-outline'}
+                color={color}
+                size={30}
+              />
+            ),
+          }}
         />
-        <Tab.Screen name="searchTab" component={SearchStack} />
+        <Tab.Screen
+          name="searchTab"
+          component={SearchStack}
+          options={{
+            tabBarIcon: ({focused, color}) => (
+              <Icon
+                name={focused ? 'food-drumstick' : 'food-drumstick-outline'}
+                color={color}
+                size={25}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
