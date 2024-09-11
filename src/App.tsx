d7e933faa9a3430f82ec9a866f2738875772edf5
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-09-11 11:56:57 +0530

    initial splash screen setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import ThemeContextProvider from './context/Theme';
 import AppNavigator from './navigation/navigator';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeContextProvider>
       <SafeAreaView style={{flex: 1}}>
