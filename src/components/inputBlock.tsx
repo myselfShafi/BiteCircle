e0fcae0d404428eaa14bafee1e2e2821c1ddabc1
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-09-15 23:48:04 +0530

    update input box

--- src/components/inputBlock.tsx
+++ src/components/inputBlock.tsx
@@ -1,15 +1,30 @@
 import React, {forwardRef} from 'react';
 import {StyleSheet, TextInput, ViewStyle} from 'react-native';
-import {TextInput as PaperTextInput, TextInputProps} from 'react-native-paper';
+import {
+  TextInput as PaperTextInput,
+  TextInputProps,
+  useTheme,
+} from 'react-native-paper';
 
-const InputBox = forwardRef<TextInput, TextInputProps>(
-  ({style, ...props}, ref): JSX.Element => {
+type InputBoxProps = TextInputProps & {
+  hasError?: boolean;
+};
+
+const InputBox = forwardRef<TextInput, InputBoxProps>(
+  ({style, hasError, ...props}, ref): JSX.Element => {
+    const theme = useTheme();
     return (
       <PaperTextInput
         theme={{roundness: 20}}
         underlineStyle={styles.underline}
-        style={[styles.container, style]}
+        style={[
+          styles.container,
+          style,
+          {backgroundColor: theme.colors.surfaceVariant},
+        ]}
         ref={ref}
+        error={hasError}
+        mode={hasError ? 'outlined' : 'flat'}
         {...props}
       />
     );
